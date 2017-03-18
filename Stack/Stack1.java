4b28a726ae0b5584c2525262bee3132d24e7ae82
SH Hridoy <shhridoy.cste@gmail.com>
2017-03-18 19:58:17 +0600

    Stack using user defined function

--- Stack/Stack1.java
+++ Stack/Stack1.java
@@ -1,29 +1,7 @@
-public class MyStack {
-   private int maxSize;
-   private long[] stackArray;
-   private int top;
+public class Stack1 {
    
-   public MyStack(int s) {
-      maxSize = s;
-      stackArray = new long[maxSize];
-      top = -1;
-   }
-   public void push(long j) {
-      stackArray[++top] = j;
-   }
-   public long pop() {
-      return stackArray[top--];
-   }
-   public long peek() {
-      return stackArray[top];
-   }
-   public boolean isEmpty() {
-      return (top == -1);
-   }
-   public boolean isFull() {
-      return (top == maxSize - 1);
-   }
    public static void main(String[] args) {
+      
       MyStack theStack = new MyStack(10); 
       theStack.push(10);
       theStack.push(20);
@@ -39,3 +17,7 @@ public class MyStack {
       System.out.println("");
    }
 }
+
+/*OUTPUT:
+50 40 30 20 10
+*/
